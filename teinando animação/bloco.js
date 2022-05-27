4c5529fcd4579fd57035d3a0f5dff98264464a3b
Filipe Leal <96875921+filipexadl@users.noreply.github.com>
2022-05-27 15:48:39 -0300

    JS

--- teinando animação/bloco.js
+++ teinando animação/bloco.js
@@ -1,11 +1,11 @@
 var quantidade = 10;
 var preco = 6.4;
 var imposto = 1.5;
-var precofinal = preco+imposto;
+var precoFinal = preco+imposto;
 
  console.log("Caneta");
  console.log(quantidade);
  console.log(preco);
  console.log(imposto);
  console.log(preco + imposto);
- console.log(precofinal)
+ console.log(precoFinal)
