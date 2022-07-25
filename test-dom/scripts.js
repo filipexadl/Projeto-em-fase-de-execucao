22412cdd4a25b6746dcddca03aa5a3e3a8433412
Filipe Leal <96875921+filipexadl@users.noreply.github.com>
2022-07-25 13:39:21 -0300

    .

---
+++ test-dom/scripts.js
@@ -0,0 +1,15 @@
+let container = document.getElementById('container')
+var soma = 5
+if (soma >7) {
+    container.style.background='black'
+    
+} else if (soma == 6){
+    container.style.background='yellow'
+}else
+container.style.background='red'
+
+
+var p = document.getElementById('demo')
+var butao = document.getElementById('butao')
+demo = Date()
+
