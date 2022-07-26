df46c5b040a4a7cb775c28bc2e54d061a320b85c
Filipe Leal <96875921+filipexadl@users.noreply.github.com>
2022-07-26 14:01:42 -0300

    .

--- test-dom/scripts.js
+++ test-dom/scripts.js
@@ -1,15 +1,14 @@
-let container = document.getElementById('container')
-var soma = 5
-if (soma >7) {
-    container.style.background='black'
+var nota1 = prompt('digite sua notas1') 
+var media = 5
+
+
+
+if (nota1 > media) {
+ 
+    document.write(`aprovado`)
+  
     
-} else if (soma == 6){
-    container.style.background='yellow'
-}else
-container.style.background='red'
-
-
-var p = document.getElementById('demo')
-var butao = document.getElementById('butao')
-demo = Date()
-
+}else{
+ 
+    document.write(`reprovado`)
+}
