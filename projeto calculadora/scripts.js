b024226335cb9e80dfbcc48c05b5e31a48f1f76b
Filipe Leal <96875921+filipexadl@users.noreply.github.com>
2022-09-05 00:50:48 -0300

    .

--- projeto calculadora/scripts.js
+++ projeto calculadora/scripts.js
@@ -1,5 +1,21 @@
 function calcular (tipo , valor) {
-console.log(tipo,valor)
-    
+    if (tipo === 'acao') {
+
+        if (valor === 'c') {
+            document.getElementById('resultado').value = '' 
+        }
+
+        if (valor === '+' || valor === '-' || valor === '+' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor
+        }
+
+        if (valor === '=') {
+           var valor_campo = document.getElementById('resultado').value
+           document.getElementById('resultado').value = (eval(valor_campo))
+          
+        }
+    } else if(tipo === 'valor') {
+     document.getElementById('resultado').value += valor
+    }
 }
 
