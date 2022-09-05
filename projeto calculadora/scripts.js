5c88a4589ead8e9d501337cb38fa88e341292136
Filipe Leal <96875921+filipexadl@users.noreply.github.com>
2022-09-05 01:15:20 -0300

    .

--- projeto calculadora/scripts.js
+++ projeto calculadora/scripts.js
@@ -5,6 +5,10 @@ function calcular (tipo , valor) {
             document.getElementById('resultado').value = '' 
         }
 
+        if (valor === 0) {
+            document.getElementById('resultado').value += 0
+        }
+
         if (valor === '+' || valor === '-' || valor === '+' || valor === '*' || valor === '/' || valor === '.') {
             document.getElementById('resultado').value += valor
         }
